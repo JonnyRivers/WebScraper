502970e7b6bfab3af23ad22f690783af0ce9815e
JonnyRivers <jonny.w.rivers@gmail.com>
2017-06-18 10:46:30 -0700

    Fixed comments in WebScaperContextFactory

--- WebScraper.Testbed/Data/WebScraperContextFactory.cs
+++ WebScraper.Testbed/Data/WebScraperContextFactory.cs
@@ -5,12 +5,12 @@ namespace WebScraper.Testbed.Data
 {
     class WebScraperContextFactory : IDbContextFactory<WebScraperContext>
     {
-        // This exists so that commands like 'dotnet ef migrations Initial -c WebScraperContext' work
+        // This exists so that commands like 'dotnet ef migrations Initial' work
 
         public const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=WebScraper;Trusted_Connection=True;";
 
         // To create the db:
-        // 'dotnet ef migrations add Initial -c WebScraper.Testbed.WebScraperContext'
+        // 'dotnet ef migrations add Initial'
         // 'dotnet ef database update Initial'
         // 'dotnet'
         public WebScraperContext Create(DbContextFactoryOptions options)
