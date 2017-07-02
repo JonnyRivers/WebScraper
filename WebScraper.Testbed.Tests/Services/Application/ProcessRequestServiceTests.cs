d6fde074e34add5c4f667bddbb583882824dd8ee
Jonny Rivers <jonny.w.rivers@gmail.com>
2017-07-01 19:50:56 -0700

    Started on ProcessRequestService

---
+++ WebScraper.Testbed.Tests/Services/Application/ProcessRequestServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebScraper.Testbed.Tests.Services.Application
+{
+    class ProcessRequestServiceTests
+    {
+    }
+}
---
+++ WebScraper.Testbed/Services/Application/ProcessRequestService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebScraper.Testbed.Services.Application
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Logging;
+    using Microsoft.EntityFrameworkCore;
+
+    using WebScraper.Testbed.Data;
+
+    public class ProcessRequestService : IProcessRequestService
+    {
+        private readonly ILogger<ProcessRequestService> m_logger;
+        private readonly WebScraperContext m_dbContext;
+
+        public ProcessRequestService(ILogger<ProcessRequestService> logger, WebScraperContext dbContext)
+        {
+            m_logger = logger;
+            m_dbContext = dbContext;
+        }
+
+        public void Dispose()
+        {
+            m_dbContext.Dispose();
+        }
+
+        public async Task ProcessRequestAsync()
+        {
+            m_logger.LogInformation("Finding next available pending page.");
+            Page nextPage = await m_dbContext.Pages
+                .FirstOrDefaultAsync(x => x.Status == Status.Pending);
+
+            if (nextPage == null)
+            {
+                m_logger.LogInformation("No pages are available to process.");
+
+                return;
+            }
+
+            nextPage.StartedAt = DateTime.UtcNow;
+            nextPage.Status = Status.Downloading;
+
+            await m_dbContext.SaveChangesAsync();
+        }
+    }
+}
