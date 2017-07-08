e1babd8c196c5a34a720ac672774f65493968598
JonnyRivers <jonny.w.rivers@gmail.com>
2017-07-08 14:51:01 -0700

    Simplified PageParseServiceTests

--- WebScraper.Testbed.Tests/Services/Core/PageParseServiceTests.cs
+++ WebScraper.Testbed.Tests/Services/Core/PageParseServiceTests.cs
@@ -1,7 +1,9 @@
 namespace WebScraper.Testbed.Tests.Services.Core
 {
+    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Text;
 
     using Microsoft.Extensions.Logging;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,22 +14,36 @@ namespace WebScraper.Testbed.Tests.Services.Core
     public class PageParseServiceTests
     {
         [TestMethod]
-        public void TestParseGuardian()
+        public void TestParsePage()
         {
             ILoggerFactory loggerFactory = new LoggerFactory();
             ILogger<PageParseService> logger = loggerFactory.CreateLogger<PageParseService>();
 
-            using (FileStream guardianContentStream = File.OpenRead("Guardian-2017-06-18.html"))
+            string link1 = "https://assets.guim.co.uk/";
+            string link2 = "https://i.guim.co.uk/";
+            string testPageContent =
+                "< !DOCTYPE html >" +
+                "< html id = \"js-context\" class=\"js-off is-not-modern id--signed-out\" data-page-path=\"/us\">" + 
+                "<head>" + 
+                "<meta charset = \"utf-8\" />" + 
+                "<link />" + // invalid link
+               $"<link rel = \"\" href = \"{link1}\" />" +
+               $"<link href=\"{link2}\"/>";
+            byte[] testPageContentBytes = Encoding.UTF8.GetBytes(testPageContent);
+
+            using (MemoryStream testPageStream = new MemoryStream(testPageContentBytes))
             {
                 var pageParseService = new PageParseService(logger);
-                Content.WebPageContent webPageContent = pageParseService.ParseWebPage(guardianContentStream);
+                Content.WebPageContent webPageContent = pageParseService.ParseWebPage(testPageStream);
 
                 // Check parsed content
                 Assert.IsNotNull(webPageContent);
                 Assert.IsNotNull(webPageContent.Links);
-                Assert.AreEqual(238, webPageContent.Links.Count());
-                Assert.AreEqual(@"https://assets.guim.co.uk/", webPageContent.Links.First().Value);
-                Assert.AreEqual(@"https://subscribe.theguardian.com/us?INTCMP=NGW_FOOTER_US_GU_SUBSCRIBE", webPageContent.Links.Last().Value);
+
+                var links = new List<Content.WebPageLink>(webPageContent.Links);
+                Assert.AreEqual(2, links.Count);
+                Assert.AreEqual(link1, links[0].Value);
+                Assert.AreEqual(link2, links[1].Value);
             }
         }
 
